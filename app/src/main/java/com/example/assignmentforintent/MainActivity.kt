fb7f5e2a46f5eefb57d72bd543a9905e6664b9b2
SujalKarale <karalesujal@gmail.com>
2024-06-16 20:56:07 +0530

    Final Commit

---
+++ app/src/main/java/com/example/assignmentforintent/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.assignmentforintent
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputLayout
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun worldCup(view: View) {
+        // get data from TextInputLayout
+        val til: TextInputLayout = findViewById(R.id.textField)
+        val s = til.editText?.text.toString()
+        val i: Intent = Intent(this, SecondActivity::class.java)
+        // Now send this data as an extra value
+        i.putExtra("DATA", s)
+        startActivity(i)
+    }
+
+    fun fifaCup(view: View) {
+        val til: TextInputLayout = findViewById(R.id.textField1)
+        val s = til.editText?.text.toString()
+        val i: Intent = Intent(this, ThirdActivity::class.java)
+        // Now send this data as an extra value
+        i.putExtra("BATA", s)
+        startActivity(i)
+    }
+}
